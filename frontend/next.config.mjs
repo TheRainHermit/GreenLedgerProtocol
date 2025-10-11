3b277fb15b32ae1b780f4acfc8417fd9a8e3cfd1
TheRainHermit <miguel.fabra211294@gmail.com>
2025-10-11 16:58:22 -0500

    Commit 2: Pruebas frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false
+    }
+    return config
+  },
+  env: {
+    NEXT_PUBLIC_CONTRACT_ADDRESS: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    NEXT_PUBLIC_ENVIO_HYPERSYNC_URL: process.env.NEXT_PUBLIC_ENVIO_HYPERSYNC_URL,
+    NEXT_PUBLIC_LIGHTHOUSE_API_KEY: process.env.NEXT_PUBLIC_LIGHTHOUSE_API_KEY,
+  }
+}
 
 export default nextConfig;
